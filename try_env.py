cd577dfdad0cb10ecb0307e6f98591d4e3b5fb04
yuvalhef <41970107+yuvalhef@users.noreply.github.com>
2019-12-28 12:11:33 +0200

    First commit

---
+++ try_env.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+
+# from stable_baselines.common.policies import MlpLnLstmPolicy
+# from stable_baselines.common.vec_env import DummyVecEnv
+# from stable_baselines import PPO2
+
+from env.TradingEnv import TradingEnv
+from util.indicators import add_indicators
+
+input_data_file = 'data/coinbase_hourly.csv'
+
+
+reward_strategy = 'sortino'
+
+# df = pd.read_csv(input_data_file)
+# df = df.drop(['Symbol'], axis=1)
+# df = df.sort_values(['Date'])
+# df = add_indicators(df.reset_index())
+#
+# train_len = int(len(df) * 0.8)
+#
+# df = df[:train_len]
+#
+# validation_len = int(train_len * 0.8)
+# train_df = df[:validation_len]
+# test_df = df[validation_len:]
+
+
+env = TradingEnv(reward_func=reward_strategy)
+env.reset()
+
+for i in range(100000):
+    j = np.random.random_integers(12)
+    next_state, rew, done, _ = env.step(j)
+    env.render()
+    print(j)
+    if done:
+        env.reset()
+
+
+env.reset()
+print('bi')
+
