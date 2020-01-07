ac621594f55f2c749d47579dd609d30975a4d5c6
hilad <hilad@dragonfly-grp.com>
2020-01-07 16:27:45 +0200

    added DTW

--- main_after_yuval_preprocessing.py
+++ main_after_yuval_preprocessing.py
@@ -4,7 +4,12 @@ import copy
 from pandas.api.types import is_numeric_dtype
 from tqdm import tqdm
 from datetime import datetime
+import pandas as pd
 from tslearn.piecewise import SymbolicAggregateApproximation
+from sklearn.cluster import KMeans
+# from fastdtw import fastdtw
+from dtaidistance import dtw
+from collections import defaultdict
 
 
 def normalize_data(train, test, cols):
@@ -35,18 +40,17 @@ def normalize_data(train, test, cols):
 
 
 def compute_sax(stock_dict, cols, n_paa_segments, n_sax_symbols):
+    print("\nComputing SAX:")
     sax = SymbolicAggregateApproximation(n_segments=n_paa_segments, alphabet_size_avg=n_sax_symbols)
+
     for stock in tqdm(list(stock_dict.keys())):
         train_stock_df = stock_dict[stock]['train']
-        test_stock_df = stock_dict[stock]['test']
         for col in cols:
             train_stock_df[col] = sax.inverse_transform(sax.fit_transform(train_stock_df[col]))[0]
-            test_stock_df[col] = sax.inverse_transform(sax.fit_transform(test_stock_df[col]))[0]
 
         stock_dict[stock]['train'] = train_stock_df
-        stock_dict[stock]['test'] = test_stock_df
 
-    return stock_dict
+    return sax, stock_dict
 
 
 def main():
@@ -59,8 +63,11 @@ def main():
     data_with_sentiment = pickle.load(file)
     file.close()
 
+    stocks_list = list(data_with_sentiment.keys())
+    data_with_sentiment = {k: v for k, v in data_with_sentiment.items() if k in stocks_list[0:5]}  # subset
+
     # Clean data:
-    for stock in tqdm(list(data_with_sentiment.keys())):
+    for stock in list(data_with_sentiment.keys()):
         train_stock_df = data_with_sentiment[stock]['train']['df']
         test_stock_df = data_with_sentiment[stock]['test']['df']
         cols = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'sentiment_1', 'sentiment_2']
@@ -68,6 +75,7 @@ def main():
         data_with_sentiment[stock]['test'] = test_stock_df[cols]
 
     # Normalize the stock prices:
+    print("\nNormalize:")
     for stock in tqdm(list(data_with_sentiment.keys())):
         train_stock_df = data_with_sentiment[stock]['train']
         test_stock_df = data_with_sentiment[stock]['test']
@@ -83,17 +91,56 @@ def main():
     max_date = datetime(2015, 10, 19).date()
     diff_in_days = (max_date - min_date).days
     paa_average_days = 5
-    n_sax_symbols = 100
+    n_sax_symbols = 10  # 5, 10
     cols = ['Open', 'High', 'Low', 'Close', 'Volume']
     days = int(round(452 / 7, 0)) * paa_average_days
     n_paa_segments = int(round(days / paa_average_days, 0))
 
-    data_with_sentiment_sax = compute_sax(stock_dict=data_with_sentiment,
-                                          cols=cols,
-                                          n_paa_segments=n_paa_segments,
-                                          n_sax_symbols=n_sax_symbols)
+    sax, data_with_sentiment_sax = compute_sax(stock_dict=data_with_sentiment,
+                                               cols=cols,
+                                               n_paa_segments=n_paa_segments,
+                                               n_sax_symbols=n_sax_symbols)
+
+    # Compute distance matrix:
+    import itertools
+    import numpy as np
+
+    distance_dict = defaultdict(lambda: defaultdict(list))
+
+    stocks_list = list(data_with_sentiment.keys())
+    combinations = list(itertools.permutations(stocks_list, 2))
+
+    for (stock1, stock2) in tqdm(combinations):
+
+        dtw_dis_list = []
+
+        for col in cols:
+            s1 = np.array(data_with_sentiment_sax[stock1]['train'][col])
+            s2 = np.array(data_with_sentiment_sax[stock2]['train'][col])
+
+            # Compute DTW distance:
+            dtw_distance = dtw.distance(s1, s2)
+            dtw_dis_list.append(dtw_distance)
+
+        distance_dict['DTW']['stock_1'].append(stock1)
+        distance_dict['DTW']['stock_2'].append(stock2)
+        distance_dict['DTW']['distance_list'].append(dtw_dis_list)
+        distance_dict['DTW']['distance_average'].append(np.mean(dtw_dis_list))
+
+    dtw_distances_df = pd.DataFrame(distance_dict['DTW'])
 
 
+
+    x = 2
+
+    # dm = np.asarray([[dist(p1, p2) for p2 in xy_list] for p1 in xy_list])
+
+    # # Cluster the stocks:
+    # n_clusters = 3
+    # km = KMeans(n_clusters=n_clusters, init='random',max_iter=300, tol=1e-04)
+    # y_km = km.fit_predict(X)
+
+    x = 3
 
 
 if __name__ == '__main__':
