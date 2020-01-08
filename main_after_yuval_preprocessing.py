a147a1d08c14a02988cb4760b96824445bdb4835
hilad <hilad@dragonfly-grp.com>
2020-01-08 22:02:39 +0200

    finish with clustering function

--- main_after_yuval_preprocessing.py
+++ main_after_yuval_preprocessing.py
@@ -10,14 +10,11 @@ from collections import defaultdict
 import itertools
 import numpy as np
 from datetime import datetime, timedelta
-from scipy.cluster.hierarchy import linkage
 import matplotlib.pyplot as plt
-from scipy.cluster import hierarchy
-from scipy.spatial.distance import squareform
-from sklearn.cluster import AgglomerativeClustering
 from sklearn.cluster import DBSCAN
-from scipy.cluster.hierarchy import dendrogram, linkage
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 import scipy.spatial.distance as ssd
+
 
 def normalize_data(train, test, cols):
     norm_train = copy.deepcopy(train)
@@ -64,6 +61,7 @@ def compute_distance_matrix(stocks_list, stocks_dict, cols, distance_metric):
     print("\nCompute {d} distance matrix:".format(d=str(distance_metric)))
     distance_dict = defaultdict(lambda: defaultdict(float))
     combinations = list(itertools.permutations(stocks_list, 2))
+    values_list = []
 
     # Remove duplicate combinations:
     no_duplicate_combinations = []
@@ -85,9 +83,14 @@ def compute_distance_matrix(stocks_list, stocks_dict, cols, distance_metric):
                 # Compute SAX distance:
                 distance = np.linalg.norm(s1 - s2)
             distance_list.append(distance)
-        distance_dict[stock1][stock2] = np.mean(distance_list)
+
+        final_distance = float(np.mean(distance_list))
+        values_list.append(final_distance)
+        distance_dict[stock1][stock2] = final_distance
 
     # Create distance matrix:
+    min = np.min(values_list)
+    max = np.max(values_list)
     distance_matrix_dict = defaultdict(lambda: defaultdict(float))
     for stock1 in stocks_list:
         for stock2 in stocks_list:
@@ -96,31 +99,38 @@ def compute_distance_matrix(stocks_list, stocks_dict, cols, distance_metric):
             else:
                 if stock1 in list(distance_dict.keys()):
                     value = distance_dict[stock1][stock2]
+                    value = (value - min) / (max-min)
+
                 else:
                     value = distance_dict[stock2][stock1]
+                    value = (value - min) / (max-min)
+
             distance_matrix_dict[stock1][stock2] = value
     distance_matrix = pd.DataFrame.from_dict(distance_matrix_dict, orient='index')
 
     return distance_matrix
 
 
-def create_clusters_from_distance_matrix(distance_matrix, n_of_clusters):
+def cluster_distance_matrix(distance_matrix, cluster_method, cluster_params):
+    print("\nClustering distance matrix:")
+    if cluster_method == 'linkage':
+        stocks = list(distance_matrix.columns)
+        dist_array = ssd.squareform(distance_matrix)
+        Z = linkage(dist_array)
 
-    stocks = list(distance_matrix.columns)
+        if cluster_params['plot']:
+            fig = plt.figure(figsize=(25, 10))
+            dn = dendrogram(Z, labels=stocks)
+            plt.show()
 
-    dist_array = ssd.squareform(distance_matrix)
-    Z = linkage(dist_array)
-    fig = plt.figure(figsize=(25, 10))
-    dn = dendrogram(Z, labels=stocks)
-    plt.show()
+        labels = fcluster(Z=Z, t=cluster_params['t'], criterion=cluster_params['criterion'])
+    else:
+        cluster = DBSCAN(**cluster_params).fit(distance_matrix)
+        labels = cluster.labels_
 
-    cluster = DBSCAN(eps=3, min_samples=1).fit_predict(distance_matrix)
-
-    labels = cluster.labels_
-
-    print("c")
-
-    distance_matrix['cluser'] = labels
+    clusters_num = len(list(set(labels)))
+    print("\nNumber of clusters with method {m}: {n}".format(m=str(cluster_method), n=str(clusters_num)))
+    distance_matrix['cluster'] = labels
 
     return distance_matrix
 
@@ -133,8 +143,33 @@ def datetime_range(start=None, end=None):
 
 def main():
     # Configs:
+    global cols
     project_url = os.path.dirname(os.path.realpath(__file__)) + '/'
     input_url = project_url + 'data/'
+
+    # Sax:
+    preform_sax = True
+    min_date = datetime(2014, 1, 2).date()
+    max_date = datetime(2015, 10, 19).date()
+    paa_average_days = 5
+    n_sax_symbols = 10  # 5, 10
+
+    # Cluster:
+    cluster_method = 'DBSCAN'  # linkage or 'DBSCAN'
+
+    fcluster_params = {
+
+        't': 10,
+        'criterion': 'distance',  # inconsistent or distance
+        'plot': False
+
+    }
+
+    DBSCAN_params = {
+        'eps': 0.5,
+        'min_samples': 2,
+        'metric': 'euclidean'
+    }
 
     # Load pickle:
     file = open(input_url + 'data_with_sentiment.pickle', 'rb')
@@ -142,8 +177,7 @@ def main():
     file.close()
 
     stocks_list = list(data_with_sentiment.keys())
-    data_with_sentiment = {k: v for k, v in data_with_sentiment.items() if k in stocks_list[0:3]}  # subset
-    stocks_list = list(data_with_sentiment.keys())
+    data_with_sentiment = {k: v for k, v in data_with_sentiment.items() if k in stocks_list[0:10]}  # subset
 
     # Clean data:
     for stock in list(data_with_sentiment.keys()):
@@ -163,47 +197,47 @@ def main():
         data_with_sentiment[stock]['train'] = norm_train
         data_with_sentiment[stock]['test'] = norm_test
 
-    # SAX transform:
-    # Configs:
-    min_date = datetime(2014, 1, 2).date()
-    max_date = datetime(2015, 10, 19).date()
-    diff_in_days = (max_date - min_date).days
-    paa_average_days = 5
-    n_sax_symbols = 10  # 5, 10
-    cols = ['Open', 'High', 'Low', 'Close', 'Volume']
-    days = int(round(452 / 7, 0)) * paa_average_days
-    n_paa_segments = int(round(days / paa_average_days, 0))
-
-    sax, data_with_sentiment_sax = compute_sax(stock_dict=data_with_sentiment,
-                                               cols=cols,
-                                               n_paa_segments=n_paa_segments,
-                                               n_sax_symbols=n_sax_symbols)
-
     # Compute distance matrix:
-    dtw_distance_matrix = compute_distance_matrix(stocks_list=list(data_with_sentiment.keys()),
+    if preform_sax:
+        diff_in_days = (max_date - min_date).days
+        cols = ['Open', 'High', 'Low', 'Close', 'Volume']
+        days = int(round(452 / 7, 0)) * paa_average_days
+        n_paa_segments = int(round(days / paa_average_days, 0))
+
+        sax, data_with_sentiment_sax = compute_sax(stock_dict=data_with_sentiment,
+                                                   cols=cols,
+                                                   n_paa_segments=n_paa_segments,
+                                                   n_sax_symbols=n_sax_symbols)
+        distance_matrix = compute_distance_matrix(stocks_list=list(data_with_sentiment_sax.keys()),
+                                                  stocks_dict=data_with_sentiment_sax, cols=cols,
+                                                  distance_metric='SAX')
+    else:
+        # Compute DTW distance matrix:
+        distance_matrix = compute_distance_matrix(stocks_list=list(data_with_sentiment.keys()),
                                                   stocks_dict=data_with_sentiment,
                                                   cols=cols, distance_metric='DTW')
-    sax_distance_matrix = compute_distance_matrix(stocks_list=list(data_with_sentiment_sax.keys()),
-                                                  stocks_dict=data_with_sentiment_sax, cols=cols, distance_metric='SAX')
 
-    # # Cluster the stocks:
-    n_of_clusters = 2
-    clusters = create_clusters_from_distance_matrix(distance_matrix=sax_distance_matrix,
-                                                    n_of_clusters=n_of_clusters)
+    # Cluster the stocks:
+    if cluster_method == 'linkage':
+        cluster_params = fcluster_params
+    else:
+        cluster_params = DBSCAN_params
+
+    distance_matrix_with_clusters = cluster_distance_matrix(distance_matrix=distance_matrix,
+                                                            cluster_method=cluster_method,
+                                                            cluster_params=cluster_params)
 
     # Create features based on clusters:
-    # cluster_list = list(set(clusters['cluser']))
-    cluster_list = [1, 2, 1, 2]
-
-    list_of_dates = list(datetime_range(start=min_date, end=max_date))
-
-    for cluster in tqdm(cluster_list):
-
-        cluster_df = pd.DataFrame(list_of_dates, columns=['Date'])
-        mean_sentiment = []
-
-        for date in cluster_df['Date']:
-            print('d')
+    # # cluster_list = list(set(clusters['cluster']))
+    # list_of_dates = list(datetime_range(start=min_date, end=max_date))
+    #
+    # for cluster in tqdm(cluster_list):
+    #
+    #     cluster_df = pd.DataFrame(list_of_dates, columns=['Date'])
+    #     mean_sentiment = []
+    #
+    #     for date in cluster_df['Date']:
+    #         print('d')
 
     print("d")
 
